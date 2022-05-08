b162977ba56f1dac872ccb88a3a773811fa57860
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-05-08 12:51:14 -0300

    Style do Componente MenuNav
    feita toda estilização do componente MenuNav

--- src/Components/MenuNav/index.jsx
+++ src/Components/MenuNav/index.jsx
@@ -1,44 +1,45 @@
-import Logo from "../../Assests/Img/logo.png"
+import Logo from "../../Assests/Img/logo.png";
+import "../../Assests/Styles/Css/menuNav.css"
 
 const MenuNav = () => {
     return(
-        <div>
-            <div>
+        <div className="container--main-menuNav">
+            <div className="menuNav-title">
                  <h1>Slaca 2019</h1>
             </div>
-            <div>
+            <div className="menuNav-logo">
                 <img src={Logo} alt="Slaca"/>
             </div>
-            <div>
+            <div className="menuNav-sidebar">
                 <nav>
                     <ul>
                         <li>
-                            <button>
+                            <button className="button-presentation">
                                 <p>Apresentação</p>
                             </button>
                         </li>
                         <li>
-                            <button>
+                            <button className="button-committees">
                                 <p>Comitês</p>
                             </button>
                         </li>
                         <li>
-                            <button>
+                            <button className="button-authors">
                                 <p>Autores</p>
                             </button>
                         </li>
                         <li>
-                            <button>
+                            <button className="button-themed">
                                 <p>Eixos temáticos</p>
                             </button>
                         </li>
                         <li>
-                            <button>
+                            <button className="button-works">
                                 <p>Trabalhos</p>
                             </button>
                         </li>
                         <li>
-                            <button>
+                            <button className="button-contact">
                                 <p>Contato</p>
                             </button>
                         </li>
