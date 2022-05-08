e8b8cf02e98332807339a1a5061d7ad325534272
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-05-08 14:06:08 -0300

    estilização componente
    Feita estilização do componente Header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,26 +1,27 @@
 import Language from "../../Assests/Img/language.png";
 import DropDown from '../../Assests/Img/dropdown.png';
-import User from "../../Assests/Img/user.png"
+import User from "../../Assests/Img/user.png";
+import "../../Assests/Styles/Css/header.css"
 
 const Header = () => {
     return(
-        <div>
-             <div>
+        <div className="container--header">
+             <div className="header-description">
                  <p>Anais do Simpósio Latino Americano de Ciências de Alimentos </p>
                  <h3>Anais do 13º Simpósio Latino Americano de Ciência de Alimentos </h3>
                  <p>ISSN: 1234-5678</p>
              </div> 
-             <div>
+             <button className="header-language">
                  <img src={Language} alt="language" />
-                 <p>pt,br</p>
+                 <p>pt, br</p>
                  <img src={DropDown} alt="DropDown" />
-             </div>
-             <div>
-                 <div>
-                     <h5>Bem vindo!</h5>
+             </button>
+             <div className="header-user">
+                 <div className="user-login">
+                     <h6>Bem vindo!</h6>
                      <p>alguem12@galoascience.com</p>
                  </div>
-                 <div>
+                 <div className="user-img">
                      <img src={User} alt="usuário" />
                  </div>
              </div>
