582599a42fde1077b8766fb982ce210badaaf23b
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-05-08 15:30:26 -0300

    Criado Componente VideoDescription
    Criado componente que fazer para da descrição da main

---
+++ src/Components/VideoDescription/index.jsx
@@ -0,0 +1,91 @@
+import UserVideo from "../../Assests/Img/user-video.png"
+
+const VideoDescription = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h2>Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP </h2>
+                </div>
+                <div>
+                    <img src={UserVideo} alt="Beatriz Christiane Melo" />
+                    <div>
+                        <h3>Beatriz Christiane Melo</h3>
+                        <p>FCA / Universidade Estadual de Campinas</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <p>Detalhes</p>
+                </div>
+                <div>
+                    <div>
+                        <ul>
+                            <li>
+                                <ul>
+                                    <li>
+                                        <p>Tipo de Apresentação: <span>Pôster.</span></p>
+                                    </li>
+                                    <li>
+                                        <p>Eixo temático: <span>Alimentação e saúde (AS).</span></p>
+                                    </li>
+                                    <li>
+                                        <p>Palavra-chaves: <span>Alimentos funcionais, alimentação escolar.</span></p>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li>
+                                        <p>Autores:</p>
+                                    </li>
+                                    <li>
+                                        <p>Galileo Galilei</p>
+                                    </li>
+                                    <li>
+                                        <p>Berta Lange de Morretes</p>
+                                    </li>
+                                    <li>
+                                        <p>Isaac Newton</p>
+                                    </li>
+                                    <li>
+                                        <p>Cesar Lattes</p>
+                                    </li>
+                                    <li>
+                                        <p>Stephen Hawking</p>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li>
+                                        <p>Universidade Estadual de Campinas</p>
+                                    </li>
+                                    <li>
+                                        <p>Universidade de são Paulo</p>
+                                    </li>
+                                    <li>
+                                        <p> Instituto Nacional de Pesquisa Espaciais</p>
+                                    </li>
+                                    <li>
+                                        <p>Universidade Federal do Rio de Janeiro</p>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <div>  
+                        <div></div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default VideoDescription
