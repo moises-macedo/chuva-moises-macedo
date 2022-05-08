02fc49bd363304e475a332b8303712e2cfc90adb
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-05-08 14:59:26 -0300

    feito estilização do Componente
    Feito estilização do componente TitleVideo

--- src/Components/TitleVideo/index.jsx
+++ src/Components/TitleVideo/index.jsx
@@ -1,24 +1,25 @@
 import { AiFillStar, } from "react-icons/ai";
 import { ImDownload3 } from "react-icons/im";
+import "../../Assests/Styles/Css/titleVideo.css"
 
 const TitleVideo = () => {
     return (
-        <div>
-            <div>
+        <div className="container--titleVideo">
+            <div className="titleVideo-title">
                 <h2>
                     Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP
                 </h2>
             </div>
-            <div>
-                <div>
-                    <button>
+            <div className="titleVideo-description">
+                <div className="description-btn">
+                    <button className="btn-download">
                         <p><ImDownload3/></p>
                         <p>Download</p>
                     </button>
-                    <button>
+                    <button className="btn-star">
                         <p><AiFillStar /></p>
                     </button>
-                    <button>
+                    <button className="btn-doi">
                         <div>
                             <p>
                                 doi
@@ -26,7 +27,7 @@ const TitleVideo = () => {
                         </div>
                     </button>
                 </div>
-                <div>
+                <div className="titleVideo-link">
                     <a href="#">Como citar esse Trabalho?</a>
                 </div>
 
